1f58aedae4b078ec32d842a6de4fc6af23f76728
unknown <iangaly.nirina@gmail.com>
2025-05-08 14:27:07 +0300

    suppr MasonryGalleryImage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,12 +7,10 @@ import about1 from "../assets/images/ariel/about-1.jpg";
 import about2 from "../assets/images/ariel/about-2.jpg";
 import about3 from "../assets/images/ariel/about-3.jpg";
 
-import MasonryImagesGallery from "../../../frontend/src/components/Image-gallery/MasonryImagesGallery";
 import Subtitle from "../../../frontend/src/shared/Subtitle";
 import ServiceList from "../services/ServiceList";
 import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 import GalleryImages from "../components/Image-gallery/GalleryImages";
-// import Testimonials from "../components/Testimonial/Testimonials";
 
 const Home = () => {
   return (
